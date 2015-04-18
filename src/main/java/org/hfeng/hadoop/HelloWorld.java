133cca9df4556526f5d56fb9bdeabdb4ac5890a9
Haoran Feng <haoran.feng@sap.com>
2015-04-18 19:46:11 +0800

    before the test, can try on docker

--- src/main/java/org/hfeng/hadoop/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package org.hfeng.hadoop;
-
-import org.apache.hadoop.io.Text;
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
