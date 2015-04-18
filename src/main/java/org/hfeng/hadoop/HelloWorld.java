e155887e81c936b2d1bb2a7cde6c4bb3fbb0e203
Haoran Feng <haoran.feng@sap.com>
2015-04-18 03:04:16 -0700

    save before apply idea

---
+++ src/main/java/org/hfeng/hadoop/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.hfeng.hadoop;
+
+import org.apache.hadoop.io.Text;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
